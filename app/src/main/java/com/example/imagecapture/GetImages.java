f61f2f795ade03022bac7629b3283ca56cb8a171
Rohit Deore <deorerohit18@gmail.com>
2020-09-29 12:05:46 +0530

    This class will fetch images from folder

---
+++ app/src/main/java/com/example/imagecapture/GetImages.java
@@ -0,0 +1,42 @@
+package com.example.imagecapture;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+
+import java.util.ArrayList;
+
+public class GetImages {
+
+
+    public static ArrayList<String> getListImages(Context context) {
+
+        ArrayList<String> imagesList = new ArrayList<>();
+        Uri uri;
+        String absoluteImagePath;
+        Cursor cursor;
+        int column_index_data, column_index_folder_name;
+
+
+        uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+
+        String[] projection = {MediaStore.MediaColumns.DATA,
+                MediaStore.Images.Media.BUCKET_DISPLAY_NAME};
+
+        String orderBy = MediaStore.Images.Media.DATE_TAKEN;
+
+        cursor = context.getContentResolver().query(uri, projection, MediaStore.Images.Media.DATA + " like ? ", new String[]{"%/storage/emulated/0/Pictures/mypic/%"}, orderBy + " DESC");
+
+        column_index_data = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+
+
+        while (cursor.moveToNext()) {
+            absoluteImagePath = cursor.getString((column_index_data));
+            imagesList.add(absoluteImagePath);
+        }
+
+
+        return imagesList;
+    }
+}
